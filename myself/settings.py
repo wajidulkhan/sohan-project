a86d87ff7588088af0e572f6ffc036398c4ecd13
WAJIDUL KHAN <wajidulkhan2001@gmail.com>
2022-01-09 17:15:00 +0600

    second time

--- myself/settings.py
+++ myself/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kb9=d3bl$ifhu-%gvrwx2p*i$&=5^9rflv64846z-127y=7g0w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["sohan-project.herokuapp.com", "127.0.0.1"]  
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,7 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = BASE_DIR/ 'static'
 STATIC_URL = 'static/'
 
 # Default primary key field type
